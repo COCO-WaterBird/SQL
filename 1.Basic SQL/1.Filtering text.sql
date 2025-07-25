b405330eb011dfba8faa9953d5a55540b6d5aeb6
COCO-WaterBird <zhangkk533@gmail.com>
2025-07-25 09:18:45 -0700

    new

--- 1.Basic SQL/1.Filtering text.sql
+++ 1.Basic SQL/1.Filtering text.sql
@@ -10,3 +10,9 @@ FROM films
 WHERE language IN ('English', 'Spanish', 'French');
 
 
+-- 3
+-- Find the title, certification, and language all films certified NC-17 or R that are in English, Italian, or Greek
+Select title, certification, language
+From films
+Where certification in ('NC-17','R')
+AND language in ('English','Italian','Greek')
