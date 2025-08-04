c68c79a00f861141d8def3dedc3ef86eb73e33f8
COCO-WaterBird <zhangkk533@gmail.com>
2025-08-04 08:16:56 -0700

    Update 3.Exploratory Data Analysis in SQL.sql

--- 1.Basic SQL/3.Exploratory Data Analysis in SQL.sql
+++ 1.Basic SQL/3.Exploratory Data Analysis in SQL.sql
@@ -32,3 +32,22 @@ SELECT company.name,tag_type.tag,tag_type.type
   -- Filter to most common type
   WHERE type='cloud';
 
+
+-- Select the original value
+SELECT profits_change,
+	   -- Cast profits_change
+       CAST(profits_change as integer) AS profits_change_int
+  FROM fortune500;
+
+-- Divide 10 by 3
+SELECT 10/3,
+       -- Cast 10 as numeric and divide by 3
+       10::numeric/3;
+
+
+SELECT '3.2'::numeric,
+       '-123'::numeric,
+       '1e3'::numeric,
+       '1e-3'::numeric,
+       '02314'::numeric,
+       '0002'::numeric;
